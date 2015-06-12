4f0e4b283c84b9e057dd6e957a17b1becc0315ff
Brett Rogers <brett@noxad.com>
2015-06-11 23:43:52 -0700

    Added user agent to API request (now required by API). Changed IP strings to IPAddress objects.

--- ip-tracker/Program.cs
+++ ip-tracker/Program.cs
@@ -18,12 +18,10 @@ namespace IpTracker
             CreateFiles(new List<FileInfo> { currentIpFile, ipHistoryFile });
 
             var currentIp = GetCurrentIp(ipLookupServiceUrl);
-            var lastRecordedIp = File.ReadAllText(currentIpFile.FullName);
+            var lastIp = GetLastRecordedIp(currentIpFile);
 
-            if (currentIp == lastRecordedIp)
-            {
+            if (currentIp.Equals(lastIp))
                 return;
-            }
 
             UpdateIpTextFiles(currentIp, currentIpFile.FullName, ipHistoryFile.FullName);
         }
@@ -46,27 +44,45 @@ namespace IpTracker
             }
         }
 
-        private static string GetCurrentIp(string url)
+        private static IPAddress GetCurrentIp(string url)
         {
-            string currentIp;
+            IPAddress ip;
 
-            using (var wb = new WebClient())
+            using (var client = new WebClient())
             {
-                currentIp = wb.DownloadString(url);
+                client.Headers.Add("user-agent", "yourUserAgentHereBot");
+                var apiResult = client.DownloadString(url);
+                
+                if (!IPAddress.TryParse(apiResult, out ip))
+                {
+                    throw new Exception(String.Format("Return value was not a valid IP address. Value returned was:{0}{1}", Environment.NewLine, apiResult));
+                }
             }
 
-            return currentIp;
+            return ip;
         }
 
-        private static void UpdateIpTextFiles(string currentIp, string currentIpFile, string ipHistoryFile)
+        private static IPAddress GetLastRecordedIp(FileInfo filePath)
         {
-            File.WriteAllText(currentIpFile, currentIp);
+            IPAddress ip;
+
+            var textFileContents = File.ReadAllText(filePath.FullName);
+
+            if (!IPAddress.TryParse(textFileContents, out ip))
+                ip = IPAddress.Parse("127.0.0.1");
+
+            return ip;
+        }
+
+        private static void UpdateIpTextFiles(IPAddress currentIp, string currentIpFile, string ipHistoryFile)
+        {
+            File.WriteAllText(currentIpFile, currentIp.ToString());
 
             var newIpLine = String.Format("{0}\t{1}\t{2}{3}", 
-                DateTime.Now.ToString("yyyy-MM-dd"), 
-                DateTime.Now.ToString("HH:mm:ss"), 
-                currentIp, 
-                Environment.NewLine);
+                                                            DateTime.Now.ToString("yyyy-MM-dd"), 
+                                                            DateTime.Now.ToString("HH:mm:ss"), 
+                                                            currentIp, 
+                                                            Environment.NewLine);
 
             File.AppendAllText(ipHistoryFile, newIpLine);
         }
